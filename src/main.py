05435968488a971bac02eef2cd2031ad1a27e97d
Kethan VA <kethan@netreo.com>
2025-05-30 01:59:12 +0530

    Added resolution changes

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ class DesktopClockApp:
     def __init__(self, root):
         self.root = root
         self.root.title("🕒 Desktop Clock & Weather")
-        self.root.geometry("800x500")
+        self.root.geometry("1024x600")
         self.root.configure(bg='white')
         self.root.resizable(False, False)
         
